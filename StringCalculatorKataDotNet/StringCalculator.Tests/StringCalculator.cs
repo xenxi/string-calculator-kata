31cec267cc59d8c0bce4a05b835b1be1c87a80ab
xenxi <antoniom.diaz.moreno@gmail.com>
2021-08-21 12:43:49 +0200

    refact

--- StringCalculatorKataDotNet/StringCalculator.Tests/StringCalculator.cs
+++ StringCalculatorKataDotNet/StringCalculator.Tests/StringCalculator.cs
@@ -7,14 +7,22 @@
             if (string.IsNullOrEmpty(inputString))
                 return 0;
 
-            if (inputString.Contains(","))
-            {
-                var numbers = inputString.Split(",");
-
-                return int.Parse(numbers[0]) + int.Parse(numbers[1]);
-            }
+            if (areTwoNumbers(inputString))
+                return sumTwoNumbers(inputString);
 
             return int.Parse(inputString);
+        }
+
+        private int sumTwoNumbers(string inputString)
+        {
+            var numbers = inputString.Split(",");
+
+            return int.Parse(numbers[0]) + int.Parse(numbers[1]);
+        }
+
+        private bool areTwoNumbers(string inputString)
+        {
+            return inputString.Contains(",");
         }
     }
 }
