febb6292e3f60e32230dc39310fbe517b931ac53
xenxi <antoniom.diaz.moreno@gmail.com>
2021-08-21 10:41:42 +0200

    add test red: return_0_for_empty_string

---
+++ StringCalculatorKataDotNet/StringCalculator.Tests/StringCalculatorShould.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace StringCalculator.Tests
+{
+    [TestFixture]
+    public class StringCalculatorShould
+    {
+        [Test]
+        public void return_0_for_empty_string()
+        {
+            var calculateValue = StringCalculator.Add(string.Empty);
+            calculateValue.Should().Be(0);
+        }
+    }
+}
