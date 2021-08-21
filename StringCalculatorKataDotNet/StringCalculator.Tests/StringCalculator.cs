9a2aa5980ab1f988cc898ea23108684073d65222
xenxi <antoniom.diaz.moreno@gmail.com>
2021-08-21 12:21:05 +0200

    refact convertNumberToString

--- StringCalculatorKataDotNet/StringCalculator.Tests/StringCalculator.cs
+++ StringCalculatorKataDotNet/StringCalculator.Tests/StringCalculator.cs
@@ -7,11 +7,7 @@
             if (string.IsNullOrEmpty(inputString))
                 return 0;
 
-
-            if (inputString == "2")
-                return 2;
-
-            return 1;
+            return int.Parse(inputString);
         }
     }
 }
