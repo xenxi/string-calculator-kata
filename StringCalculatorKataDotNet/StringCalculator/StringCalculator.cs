92b24ab82e5cd4679a782564b79610090b88dae9
xenxi <antoniom.diaz.moreno@gmail.com>
2021-08-22 09:34:59 +0200

    refact

--- StringCalculatorKataDotNet/StringCalculator/StringCalculator.cs
+++ StringCalculatorKataDotNet/StringCalculator/StringCalculator.cs
@@ -5,30 +5,16 @@ namespace StringCalculatorKata
 {
     public class StringCalculator
     {
-        private readonly string[] _default_delimiters;
-
-        public StringCalculator()
-        {
-            _default_delimiters = new string[] { ",", "\n" };
-        }
-
         public int Add(string inputString)
         {
             if (string.IsNullOrEmpty(inputString))
                 return 0;
 
-            (var delimiters, var cleanInputString) = GetDelimitersAndCleanInputString(inputString);
+            var delimitedNumbersString = new DelimitedNumbersString(inputString);
 
-            var numbers = GetNumbers(new DelimitedNumbersString(cleanInputString, delimiters));
+            var numbers = GetNumbers(delimitedNumbersString);
 
             return numbers.Sum();
-        }
-
-        private (string[] delimiters, string cleanInputString) GetDelimitersAndCleanInputString(string inputString)
-        {
-            var delimiters = new DelimitedNumbersString(inputString);
-
-            return (delimiters.delimiters, delimiters.inputString);
         }
 
         private Numbers GetNumbers(DelimitedNumbersString delimitedNumbersString)
